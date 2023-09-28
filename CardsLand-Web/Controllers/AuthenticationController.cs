67a697653a0a46b7d79ce8b0749b0459589eb598
Esteban Sanchez <easanrui@gmail.com>
2023-09-28 16:50:04 -0600

    First Commit

---
+++ CardsLand-Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardsLand_Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        private readonly ILogger<AuthenticationController> _logger;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult AuthLogin()
+        {
+            return View();
+        }
+        public IActionResult AuthRegister()
+        {
+            return View();
+        }
+        public IActionResult AuthRecoverPW()
+        {
+            return View();
+        }
+        public IActionResult Auth404()
+        {
+            return View();
+        }
+        public IActionResult Auth500()
+        {
+            return View();
+        }
+    }
+}
