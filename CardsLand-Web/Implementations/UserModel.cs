c51cfb8df99802666fc50b7cf113b07437ea446b
Esteban Sanchez <easanrui@gmail.com>
2023-11-11 22:30:23 -0600

    Fix structure

---
+++ CardsLand-Web/Implementations/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using CardsLand_Web.Entities;
+using CardsLand_Web.Interfaces;
+
+namespace CardsLand_Web.Implementations
+{
+    public class UserModel : IUserModel
+    {
+
+        Task<ApiResponse<UserEnt>> IUserModel.Login(UserEnt entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ApiResponse<UserEnt>> IUserModel.RegisterAccount(UserEnt entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
