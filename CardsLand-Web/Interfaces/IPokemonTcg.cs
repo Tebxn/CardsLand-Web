8bb4db320bd8e10ab4b9061e151d3f350d795bb3
Esteban Sanchez <easanrui@gmail.com>
2023-11-15 20:59:48 -0600

    get cards from pokemonApi

---
+++ CardsLand-Web/Interfaces/IPokemonTcg.cs
@@ -0,0 +1,12 @@
+﻿using CardsLand_Web.Entities;
+using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Base;
+using PokemonTcgSdk.Standard.Infrastructure.HttpClients.Cards;
+
+namespace CardsLand_Api.Interfaces
+{
+    public interface IPokemonTcg
+    {
+        Task<ApiResourceList<Card>> GetAllCards();
+        Task<List<CardEnt>> GetSpecificCardbyName(string pokemonCardName);
+    }
+}
