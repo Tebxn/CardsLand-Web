4c267c6ba9498f6a2baf8f098d8565dc74e3f7fa
Esteban Sanchez <easanrui@gmail.com>
2023-11-16 13:17:52 -0600

    Working on register account

--- CardsLand-Web/Entities/UserEnt.cs
+++ CardsLand-Web/Entities/UserEnt.cs
@@ -6,10 +6,11 @@
         public string User_Nickname { get; set; } = string.Empty; //unique
         public string User_Email { get; set; } = string.Empty; //unique
         public string User_Password { get; set; } = string.Empty;
-        public bool User_IsAdmin{ get; set; }
+        public bool User_IsAdmin { get; set; }
         public bool User_State { get; set; }
-        public bool User_Password_IsTemp { get; set; }
-        public string? UserToken { get; set; } = string.Empty;
+        public string? User_Activation_Code { get; set; }
+        public bool User_Need_AC { get; set; }
+        public string UserToken { get; set; }
 
     }
 }
