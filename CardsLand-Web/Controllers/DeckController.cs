f5feb83deb8709efa2d46ba1d47a4b8f9eb7211e
Esteban Sanchez <easanrui@gmail.com>
2023-11-27 00:11:14 -0600

    Show user decks

--- CardsLand-Web/Controllers/DeckController.cs
+++ CardsLand-Web/Controllers/DeckController.cs
@@ -1,12 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CardsLand_Web.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CardsLand_Web.Controllers
 {
     public class DeckController : Controller
     {
+        private readonly IDeckModel _deckModel;
+
+        private readonly IHttpContextAccessor _HttpContextAccessor;
+
+
+        public DeckController(IDeckModel deckModel, IHttpContextAccessor httpContextAccessor)
+        {
+            _deckModel = deckModel;
+            _HttpContextAccessor = httpContextAccessor;
+
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MisMazos() 
+        {
+            try
+            {
+                long userId = long.Parse(_HttpContextAccessor.HttpContext.Session.GetString("UserId"));
+                var apiResponse = await _deckModel.GetAllUserDecks(userId);
+
+                if (apiResponse.Success)
+                {
+                    var data = apiResponse.Data;
+                    if (data != null)
+                    {
+                        return View(data);
+                    }
+                    else
+                    {
+                        ViewBag.MensajePantalla = "Ha ocurrido un error para mostrar tus mazos";
+                        return View();
+                    }
+                }
+                else
+                {
+                    ViewBag.MensajePantalla = "No se logro conexion con el servidor";
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.MensajePantalla = "Error al cargar los datos";
+                return View();
+            }
         }
 
         [HttpGet]
---
+++ CardsLand-Web/Interfaces/IDeckModel.cs
@@ -0,0 +1,9 @@
+﻿using CardsLand_Web.Entities;
+
+namespace CardsLand_Web.Interfaces
+{
+    public interface IDeckModel
+    {
+        Task<ApiResponse<List<DeckEnt>>> GetAllUserDecks(long userId);
+    }
+}
